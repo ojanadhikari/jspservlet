810720d44696f9608dea92ad92af13340cdf7df7
ojan <ojan.adhikari@wealthdoor.com>
2024-07-24 07:32:34 +0545

    changes

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/webapp/WEB-INF/web.xml,2/e/2e22b6edd746fa3dc40ad7276c87c8d87d72a392
+g
+7src/main/java/com/bo/jspservelet/HelloWorldServlet.java,3/d/3d99fa6823f7c1b9181b6e34a0cc3a1dd5a0a949
+Q
+!src/main/webapp/WEB-INF/index.jsp,5/3/53f2773448a6c5938ccf5b998853f2da8ff8c2d4
---
+++ src/main/java/com/bo/jspservelet/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package com.bo.jspservelet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/m")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
+}
